21a87899f5260de46b93f2ea4b6a38e7035abc47
unknown <nisha.pawar2231@gmail.com>
2024-05-12 16:17:29 +0530

    first commit

---
+++ xcounter/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count,setCount]=useState(0);
+  
+  const increment=()=>{
+    setCount(count+1);
+  }
+  const decrement=()=>{
+    setCount(count-1);
+  }
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <p>Count:{count}</p>
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
